df3828a26e728236b2d52aa1fc361c36c2a943ba
Zac Gordon <email@zacgordon.com>
2019-03-03 22:36:32 -0500

    NEW: Add material design album

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Website from "./Website";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <Website />;
+  }
+}
+
+export default App;
